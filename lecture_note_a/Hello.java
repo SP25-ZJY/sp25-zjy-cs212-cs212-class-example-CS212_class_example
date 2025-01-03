19e2c7f0ca39739ed32faa7171d76b1eb80321a8
Zelalem Jembre Yalew <zizutg@gmail.com>
2025-01-03 15:57:49 -0500

    Init

--- lecture_note_a/Hello.java
+++ lecture_note_a/Hello.java
@@ -3,5 +3,6 @@ package lecture_note_a;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("\nHello World");
+        System.out.println("Let's see class template update");
     }
 }
