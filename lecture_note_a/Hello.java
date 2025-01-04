69c20271a6b80f5073aa1f151df2e6538ce7f0dc
Zelalem Jembre Yalew <zizutg@gmail.com>
2025-01-04 12:38:10 -0500

    Merged

--- lecture_note_a/Hello.java
+++ lecture_note_a/Hello.java
@@ -3,6 +3,5 @@ package lecture_note_a;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("\nHello World");
-        System.out.println("Let's see class template update");
     }
 }
