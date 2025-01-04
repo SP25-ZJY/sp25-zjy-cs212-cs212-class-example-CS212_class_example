48bd043a6d288572f9f0adc1bc880d801f17cfcb
Zelalem Jembre Yalew <zizutg@gmail.com>
2025-01-04 12:52:24 -0500

    From main

--- lecture_note_a/Hello.java
+++ lecture_note_a/Hello.java
@@ -2,6 +2,6 @@ package lecture_note_a;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello CS212");
     }
 }
